bc33e6f83e3acfb6ce5b145914c051ebd3b55a22
TheBricktop <kaszkiet0@gmail.com>
2024-02-26 13:32:31 +0100

    Updated with  Nissim fixes

--- lib/armature.py
+++ lib/armature.py
@@ -1,19 +1,42 @@
 import bpy
 
 def restore_armature_layers(layers_select):
-    # restore the armature layers visibility
-    for i in range(0, 32):
-        bpy.context.active_object.data.layers[i] = layers_select[i]
+    # Check if the active object is an armature
+    if bpy.context.active_object.type != 'ARMATURE':
+        return
+
+    armature = bpy.context.active_object.data
+
+    # Check Blender version
+    if bpy.app.version < (4, 0, 0):
+        # For Blender v3 and earlier, restore layers visibility
+        for i in range(len(armature.layers)):
+            armature.layers[i] = layers_select[i]
+    else:
+        # For Blender v4, restore collections visibility
+        for i, collection in enumerate(armature.collections):
+            if i < len(layers_select):
+                collection.is_visible = layers_select[i]
         
-        
+
 def enable_all_armature_layers():
-    # enable all layers
-    # and return the list of each layer visibility
-    _layers = bpy.context.active_object.data.layers
+    # Check if the active object is an armature
+    if bpy.context.active_object.type != 'ARMATURE':
+        return []
+
+    armature = bpy.context.active_object.data
     layers_select = []
-    for i in range(0, 32):
-        layers_select.append(_layers[i])
-    for i in range(0, 32):
-        bpy.context.active_object.data.layers[i] = True
+
+    # Check Blender version
+    if bpy.app.version < (4, 0, 0):
+        # For Blender v3 and earlier, use layers
+        for i in range(len(armature.layers)):
+            layers_select.append(armature.layers[i])
+            armature.layers[i] = True
+    else:
+        # For Blender v4, use collections
+        for collection in armature.collections:
+            layers_select.append(collection.is_visible)
+            collection.is_visible = True
 
     return layers_select
